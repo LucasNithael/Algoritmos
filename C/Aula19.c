68b64aa68a80e12be3b98334ff20d8b8e587b841
LucasNithael <94084548+LucasNithael@users.noreply.github.com>
2022-06-21 22:22:08 -0300

    Aula19

---
+++ C/Aula19.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void append_int(int a[], int *tamanho, int capacidade, int x, int *erro){
+    *erro = 1;
+    if(*tamanho!=capacidade){
+        a[(*tamanho)++]=x;
+        *erro=0;
+    }
+}
+
+int main(){
+    int capacidade=5, a[5], tamanho=0;
+    int i, erro;
+    for(i=0; i < 10; i++){
+        append_int(a, &tamanho, capacidade, i*3, &erro);
+        if(erro==1){
+            printf("Erro de memória! \n");
+            break;
+        }
+    } 
+    printf("{");
+        for(i = 0; i < tamanho; i++){
+            printf("%d ", a[i]);
+        }
+        printf("}");
+    return 0;
+}
